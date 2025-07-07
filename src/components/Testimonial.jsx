fdf074f75c0c18c78e06ce4b7741a83325745d59
Thar <thahtetaung123@gmail.com>
2025-07-07 18:35:35 -0500

    Fixed testimonial mobile

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -10,14 +10,13 @@ export default function Testimonial() {
                     <img src={TestimonialImg} alt="" className="w-full h-[560px] object-cover object-center" />
                     <div className="absolute">
                         <div className="flex items-center justify-center flex-col space-y-2" >
-                            <div className="w-[600px] text-left">
+                            <div className="w-[300px] md:w-[500px] text-left px-2">
                                 <p className="text-black leading-8 italic">
                                     “I believe photos are not only photographs but also tell a story and hold memories that inspire us to cherish every moment. I really want to capture my special moments, and I found Yu, a true artist who embodies professionalism, passion, and patience in photography. I'm so grateful to have the opportunity to capture my special moment with her, and every time I see my photos, I always feel uplifted and satisfied.”
                                 </p>
                                 <p className='text-black text-right'>
                                     - Seng Lon (Pre-wedding)
                                 </p>
-                                
                             </div>
 
                         <Link to='/hireme'>
